49e7899dea74da1c0231a35957f9d969147d12da
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-09-15 09:00:00 +0300

    feat(back-end): swagger

---
+++ backend/src/main/java/buzzspire/helpdesk/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package buzzspire.helpdesk.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean(name = "usersMicroserviceOpenAPI")
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("BuzzSpire Help Desk")
+                        .description("BuzzSpire Help Desk API Documentation")
+                        .version("2023.0915.0"));
+    }
+}
