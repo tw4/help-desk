56599bbd685e049f4ddc6fa693dd5c98dd64865b
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-09-25 13:46:28 +0300

    feat(frontend): login design

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
 
 const Home = () => {
+  const form = useForm();
   return (
-    <main>
-      <Button variant="outline">Click me</Button>
+    <main className="max-w[1440px] h-[100vh]  flex flex-row items-center justify-center">
+      <div>
+        <Form {...form}>
+          <form>
+            <Label>Email</Label>
+            <Input type="email" placeholder="Email" />
+            <Label>Password</Label>
+            <Input type="password" placeholder="Password" />
+            <Button className="mt-5">Sign In</Button>
+          </form>
+        </Form>
+      </div>
     </main>
   );
 };
