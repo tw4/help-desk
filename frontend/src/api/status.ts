3f4a76bb888a483ca99ac4d324acb418e4e820ec
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-10-31 20:01:31 +0300

    feat(frontend): status list

--- frontend/src/api/status.ts
+++ frontend/src/api/status.ts
@@ -1,7 +1,8 @@
 import { TicketStatusEndpoints } from "@/enums/APIEnum";
 import axios from "axios";
 
-export const addStatus = async (token: string, status: string) => {
+// add ticket status
+export const addSTickettatus = async (token: string, status: string) => {
   const res = await axios
     .post(
       TicketStatusEndpoints.AddTicketStatus,
@@ -14,6 +15,42 @@ export const addStatus = async (token: string, status: string) => {
         },
       }
     )
+    .then((res) => {
+      if (res.data.success) {
+        return res.data;
+      } else {
+        return res.data;
+      }
+    });
+  return res;
+};
+
+// get ticket status list
+export const getTicketStatusList = async (token: string) => {
+  const res = await axios
+    .get(TicketStatusEndpoints.GetAllTicketStatus, {
+      headers: {
+        token: token,
+      },
+    })
+    .then((res) => {
+      if (res.data.success) {
+        return res.data.data;
+      } else {
+        return null;
+      }
+    });
+  return res;
+};
+
+// delete ticket status
+export const deleteTicketStatus = async (token: string, id: number) => {
+  const res = await axios
+    .delete(TicketStatusEndpoints.DeleteTicketStatus + id, {
+      headers: {
+        token: token,
+      },
+    })
     .then((res) => {
       if (res.data.success) {
         return res.data;
