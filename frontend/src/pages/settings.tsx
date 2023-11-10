27516a0a6b7117e4841fa22e9f77cc1fc53e5fc1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-10 10:48:52 +0300

    basic info form design

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -1,18 +1,30 @@
 import { getUser } from "@/api/user";
 import MainLayout from "@/components/layout/MainLayout";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { Role } from "@/enums/Role";
 import { useAuth } from "@/hook/Auth";
 import { useAppDispatch, useAppSelector } from "@/hook/Redux";
 import { addUser } from "@/store/features/user/userSlice";
 import { User } from "@/types/userType";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { TypeOf, z } from "zod";
 
 // basic info form schema
 const basicInfoFormSchema = z.object({
-  firstName: z.string().min(2, "First name must be at least 2 characters"),
-  lastName: z.string().min(2, "Last name must be at least 2 characters"),
+  name: z.string().min(2, "First name must be at least 2 characters"),
+  surname: z.string().min(2, "Last name must be at least 2 characters"),
   email: z.string().email("Invalid email address"),
   role: z.string(),
 });
@@ -31,10 +43,20 @@ const Settings = () => {
   const router = useRouter();
 
   // redux state
-  const user: User = useAppSelector((state) => state.user.value as User);
+  let user: User = useAppSelector((state) => state.user.value as User);
 
   // redux dispatch
   const dispatch = useAppDispatch();
+
+  // basic info form
+  const basicInfoForm = useForm<z.infer<typeof basicInfoFormSchema>>({
+    resolver: zodResolver(basicInfoFormSchema),
+    defaultValues: {
+      name: "",
+      surname: "",
+      email: "",
+    },
+  });
 
   useEffect(() => {
     if (!user) {
@@ -48,9 +70,62 @@ const Settings = () => {
     }
   }, []);
 
+  // basit info form submit
+  const basicInfoFormSubmit = (data: z.infer<typeof basicInfoFormSchema>) => {
+    console.log(data);
+  };
+
   return (
     <MainLayout>
       <h1 className="text-3xl font-bold mt-2">My Account</h1>
+      {user && (
+        <Form {...basicInfoForm}>
+          <form
+            onSubmit={basicInfoForm.handleSubmit(basicInfoFormSubmit)}
+            className="border-2 border-r-gray-200 rounded-xl p-4 mt-4 space-y-3">
+            <FormField
+              control={basicInfoForm.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>name</FormLabel>
+                  <FormControl>
+                    <Input placeholder={user.name} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={basicInfoForm.control}
+              name="surname"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>surname</FormLabel>
+                  <FormControl>
+                    <Input placeholder={user.surname} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={basicInfoForm.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>email</FormLabel>
+                  <FormControl>
+                    <Input placeholder={user.email} {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className="mt-4">Update</Button>
+          </form>
+        </Form>
+      )}
     </MainLayout>
   );
 };
