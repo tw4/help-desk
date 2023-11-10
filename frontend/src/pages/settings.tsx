8040314a6ee26c15702163ad9c718db04f57f225
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-10 11:15:59 +0300

    remove unuse import

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useToast } from "@/components/ui/use-toast";
-import { Role } from "@/enums/Role";
 import { useAuth } from "@/hook/Auth";
 import { useAppDispatch, useAppSelector } from "@/hook/Redux";
 import { addUser } from "@/store/features/user/userSlice";
