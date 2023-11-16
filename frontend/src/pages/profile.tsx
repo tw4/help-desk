d2c317360294e9cc853afefc6999d4f69cc3bd99
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-11-16 16:00:19 +0300

    create profile page

---
+++ frontend/src/pages/profile.tsx
@@ -0,0 +1,40 @@
+import MainLayout from "@/components/layout/MainLayout";
+import { useAppDispatch, useAppSelector } from "@/hook/Redux";
+import { useAuth } from "@/hook/Auth";
+import { useRouter } from "next/router";
+import { User } from "@/types/userType";
+import { useEffect } from "react";
+import { getUser } from "@/api/user";
+import { addUser } from "@/store/features/user/userSlice";
+
+const Profile = () => {
+  // auth
+  useAuth();
+
+  // router
+  const router = useRouter();
+
+  // redux state
+  const user: User = useAppSelector((state) => state.user).value as User;
+
+  // redux dispatch
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (!user) {
+      getUser(localStorage.getItem("token") || "").then((res) =>
+        dispatch(addUser(res))
+      );
+    }
+  }, []);
+
+  return (
+    <MainLayout>
+      <div className="container">
+        <h1>Profile</h1>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Profile;
